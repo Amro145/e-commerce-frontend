9d5aae3fa66ea209c6c336375628dbb077fd7aa4
Amro145 <amroaltayeb14@gmail.com>
2026-04-14 19:09:44 +0200

    style: fix import spacing in Pay.jsx

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -3,6 +3,7 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { LaptopMinimalCheck, Loader2, ShoppingCart } from "lucide-react";
+
 import Image from "next/image";
 
 const Pay = ({ cart }) => {
